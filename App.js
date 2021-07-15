8d451cbd968001c676f66aed45d9ac6eb40526d4
SamKap21 <kapoorvinit94@gmail.com>
2021-07-15 11:24:55 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  var name ="React Native";
+  return (
+    <View>
+      <Text>Hello {name}</Text>
+    </View>
+  );
+}
+
+export default App;
